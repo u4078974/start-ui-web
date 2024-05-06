2bfb4f13336e231afb52792458ccee9d45b124a7
Noé TATOUD <noe@bearstudio.fr>
2024-05-06 14:51:03 +0200

    refactor(file-upload): Remove hook abstraction layer and regroup feature's files

--- src/features/account/AccountProfileForm.tsx
+++ src/features/account/AccountProfileForm.tsx
@@ -19,7 +19,7 @@ import {
   zFormFieldsAccountProfile,
 } from '@/features/account/schemas';
 import { useAvatarUpload } from '@/features/account/useAvatarUpload';
-import { useUploadFile } from '@/hooks/useUploadFile';
+import { useAvatarFetch, useAvatarUpload } from '@/features/account/service';
 import {
   AVAILABLE_LANGUAGES,
   DEFAULT_LANGUAGE_KEY,
@@ -33,9 +33,7 @@ export const AccountProfileForm = () => {
     staleTime: Infinity,
   });
 
-  const accountAvatar = useFetchAvatar(account.data?.image || '', {
-    enabled: !!account?.data?.image,
-  });
+  const accountAvatar = useAvatarFetch(account.data?.image || '');
 
   const toastSuccess = useToastSuccess();
   const toastError = useToastError();
